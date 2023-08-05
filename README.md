03785860623f89f9180002d24f1753d5fab90b52
Vishwa Patel <vishwa.patel@medibank.com.au>
2023-08-05 11:18:43 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TestFrameworkP
+
+A description of this package.
---
+++ Sources/TestFrameworkP/TestFrameworkP.swift
@@ -0,0 +1,6 @@
+public struct TestFrameworkP {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
